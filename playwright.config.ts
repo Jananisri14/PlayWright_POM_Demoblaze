900a59de16868df95343a30c19055ddfabfc7b00
Jananisri14 <jananisathishkumar380@gmail.com>
2026-07-01 08:18:40 +0530

    Added Report

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,11 +10,15 @@ dotenv.config({
 export default defineConfig({
   testDir: './tests',
 
+
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [
+  ['html'],
+  ['allure-playwright']
+],
 
   use: {
     baseURL: process.env.BASE_URL,
